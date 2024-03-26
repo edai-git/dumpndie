dfb9e3919dcd165549d18e68c79aba4dc3ed4780
edyKurnia <edy.kurnia996@gmail.com>
2024-03-26 13:51:01 +0700

    rem test

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.dump = exports.dd = void 0;
 /**
  * The `dd` function is a debugging helper function that prints each parameter to the console and then stops script execution.
  *
@@ -10,11 +11,30 @@ Object.defineProperty(exports, "__esModule", { value: true });
  * @param data The parameters you want to print. You can provide any number of parameters, and each parameter will be printed to the console separately.
  */
 function dd(...data) {
+    console.log(`####--DUMP & DIE--####`);
     data.forEach((item, index) => {
         console.log(`------param ${index + 1}-----`);
         console.dir(item, { depth: null, colors: true });
     });
     process.exit();
 }
+exports.dd = dd;
+/**
+ * The `dump` function is a debugging helper function that prints each parameter to the console.
+ *
+ * This function accepts any number of parameters of any type. Each parameter is printed to the console with a label indicating the order of the parameter.
+ *
+ * Unlike the `dd` function, this function does not stop script execution after printing the parameters. This means that after you call this function, any code that comes after it will still be executed.
+ *
+ * @param data The parameters you want to print. You can provide any number of parameters, and each parameter will be printed to the console separately.
+ */
+function dump(...data) {
+    console.log(`####--DUMP--####`);
+    data.forEach((item, index) => {
+        console.log(`------param ${index + 1}-----`);
+        console.dir(item, { depth: null, colors: true });
+    });
+}
+exports.dump = dump;
+module.exports = { dd, dump };
 exports.default = dd;
-module.exports = dd;
--- test.ts
+++
@@ -1,5 +0,0 @@
-// import {dd as dm, dump} from './src/index'
-const { dd, dump } = require("./src/index");
-
-dump(1, 2, 3);
-dd("dai");
